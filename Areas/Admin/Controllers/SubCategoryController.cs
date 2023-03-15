a0d2522e7311fc0a7d90e8530a238da754559310
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-03-16 07:35:10 +0800

    #88

---
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Spice_tedliu.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice_tedliu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategroy.Include(s=>s.Category).ToListAsync();
+            return View(subCategories);
+        }
+    }
+}
