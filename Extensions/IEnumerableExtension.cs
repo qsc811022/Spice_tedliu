0a2665c0b0b5ce9de834a197f62e3634951811db
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-03-18 17:27:12 +0800

    92

---
+++ Extensions/IEnumerableExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Spice_tedliu.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items,int selectedValue)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text=item.GetPeopertyValue("Name"),
+                       Value=item.GetPeopertyValue("Id"),
+                       Selected=item.GetPeopertyValue("Id").Equals(selectedValue.ToString())
+
+                   };
+        }
+    }
+}
---
+++ Extensions/ReflectionExtension1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice_tedliu.Extensions
+{
+    public static class ReflectionExtension1
+    {
+        public static string GetPeopertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item,null).ToString();
+        }
+    }
+}
