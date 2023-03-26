22deac8842125bb4e39c968d76b7dd43fbfb9cd8
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-03-26 21:11:11 +0800

    Add tiny api

--- Areas/Admin/Controllers/MenuItemController.cs
+++ Areas/Admin/Controllers/MenuItemController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using Spice_tedliu.Data;
+using Spice_tedliu.Models.ViewModels;
 
 using System;
 using System.Collections.Generic;
@@ -17,17 +18,38 @@ namespace Spice_tedliu.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IHostingEnvironment _hostingEnviroment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get;set;}
+
         public MenuItemController(ApplicationDbContext db,IHostingEnvironment hostingEnvironment)
         {
             _db=db;
             _hostingEnviroment=hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category=_db.Categroy,
+                MenuItem=new Models.MenuItem()
+            };
 
         }
 
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
-            return View();
+            return View(menuItems);
         }
+
+
+        //GET- CREATE
+
+        public IActionResult Create()
+        {
+
+            return View(MenuItemVM);
+        }
+
+
+
+
     }
 }
