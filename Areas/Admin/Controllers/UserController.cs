c8eabd75667fc2ce955decae55a6d634a11af848
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-04-09 22:05:04 +0800

    #145

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Spice_tedliu.Data;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice_tedliu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
