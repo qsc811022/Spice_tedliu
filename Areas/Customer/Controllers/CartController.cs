ac2760fbcf61140570eae63172f87c9e6480021a
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-05-01 22:30:45 +0800

    #166 Add CartController Index

---
+++ Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Spice_tedliu.Data;
+using Spice_tedliu.Models;
+using Spice_tedliu.Models.ViewModels;
+using Spice_tedliu.Utility;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Spice_tedliu.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderDetailsCart detailCart { get;set;}
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+            if (cart != null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach (var list in detailCart.listCart)
+            {
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+                detailCart.OrderHeader.OrderTotal = detailCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+                list.MenuItem.Description = SD.ConvertToRawHtml(list.MenuItem.Description);
+                if (list.MenuItem.Description.Length > 100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+            detailCart.OrderHeader.OrderTotalOriginal = detailCart.OrderHeader.OrderTotal;
+
+            if (HttpContext.Session.GetString(SD.ssCouponCode) != null)
+            {
+                detailCart.OrderHeader.CouponCode = HttpContext.Session.GetString(SD.ssCouponCode);
+                var couponFromDb = await _db.Coupon.Where(c => c.Name.ToLower() == detailCart.OrderHeader.CouponCode.ToLower()).FirstOrDefaultAsync();
+                detailCart.OrderHeader.OrderTotal = SD.DiscountedPrice(couponFromDb, detailCart.OrderHeader.OrderTotalOriginal);
+            }
+
+
+            return View(detailCart);
+
+        }
+    }
+}
